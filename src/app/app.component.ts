866bed79a97d2e191b3d622e83af30c0e5aeb944
sagarmanas <42737962+sagarmanas@users.noreply.github.com>
2022-01-10 23:21:02 +0530

    First code change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Developers Rock';
+  title = 'Developers Rock';
 }
